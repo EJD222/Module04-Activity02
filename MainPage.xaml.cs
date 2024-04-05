4682e30abf2208d01df136f37d0bf260f8bc18e7
EL-J Diaz <91679071+EJD222@users.noreply.github.com>
2024-04-05 14:38:22 +0800

    Add Module 04 - Activity 02 files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module04Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnTabPageClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new TabbedPageSample());
+        }
+
+        private async void OnCarouselPageClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new CarouselPageSample());
+        }
+    }
+}
